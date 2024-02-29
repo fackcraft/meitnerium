0896c8cfefbd4f5a09a843f7b8ab69396aba361c
Ming Yue <mingfengpigeon@gmail.com>
2024-03-01 00:29:35 +0800

    Add UI elements and project configuration
    Added necessary build configurations and dependencies for the repository. Created a basic UI layout with a scrollable message list and an unused aircraft model displayed in a 3D view.

--- README.md
+++ README.md
@@ -4,6 +4,19 @@ This project is in development. Builtin AI is just a test and will be removed in
 
 FCWSLIB is also its own project, this will not be merged into Kalmium for the foreseeable future.
 
+# Build
+
+```shell
+conda install conda-forge::pyside6
+conda install conda-forge::qt6-wayland
+conda install conda-forge::qt6-3d
+```
+
+```shell
+cd assets
+pyside6-rcc resources.qrc -o resources.py
+```
+
 # Contribute
 
 Move to Kalmium.
--- main.py
+++ main.py
@@ -1,7 +1,70 @@
-import sys
 
-from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QWidget, QPushButton
-from PySide6.QtGui import QResizeEvent
+import sys
+from enum import Enum
+
+from PySide6.QtCore import QUrl
+from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QWidget, QPushButton, QScrollArea, \
+    QLabel
+from PySide6.QtGui import QResizeEvent, QVector3D, QQuaternion
+from PySide6.Qt3DExtras import Qt3DExtras
+from PySide6.Qt3DCore import Qt3DCore
+from PySide6.Qt3DRender import Qt3DRender
+
+
+# noinspection PyUnresolvedReferences
+import assets.resources
+
+
+class Aircraft(Qt3DExtras.Qt3DWindow):
+    def __init__(self) -> None:
+        super().__init__()
+
+        # camera
+        self.camera().lens().setPerspectiveProjection(45, 16 / 9, 0.1, 1000)
+        self.camera().setPosition(QVector3D(0, 0, 40))
+        self.camera().setViewCenter(QVector3D(0, 0, 0))
+
+        # root entity
+        root_entity = Qt3DCore.QEntity()
+
+        # material
+        material = Qt3DExtras.QPhongMaterial(root_entity)
+
+        self.setRootEntity(root_entity)
+
+        self.torusEntity = Qt3DCore.QEntity(root_entity)
+        self.torusMesh = Qt3DExtras.QTorusMesh()
+        self.torusMesh.setRadius(5)
+        self.torusMesh.setMinorRadius(1)
+        self.torusMesh.setRings(100)
+        self.torusMesh.setSlices(20)
+
+        self.torusTransform = Qt3DCore.QTransform()
+        self.torusTransform.setScale3D(QVector3D(1.5, 1, 0.5))
+        self.torusTransform.setRotation(QQuaternion.fromAxisAndAngle(QVector3D(1, 0, 0), 45))
+
+        self.torusEntity.addComponent(self.torusMesh)
+        self.torusEntity.addComponent(self.torusTransform)
+        self.torusEntity.addComponent(material)
+
+
+        # aircraft
+        entity = Qt3DCore.QEntity(root_entity)
+        mesh: Qt3DRender.QMesh = Qt3DRender.QMesh(root_entity)
+        mesh.setSource(":aircraft.obj")
+
+        transform = Qt3DCore.QTransform()
+        transform.setScale3D(QVector3D(1.5, 1, 0.5))
+        transform.setRotation(QQuaternion.fromAxisAndAngle(QVector3D(1, 0, 0), 45))
+
+        entity.addComponent(mesh)
+        entity.addComponent(transform)
+        entity.addComponent(material)
+
+
+class Message(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
 
 
 class Main(QWidget):
@@ -10,16 +73,29 @@ class Main(QWidget):
     def __init__(self) -> None:
         super().__init__()
 
+        self.init_ui()
+
+    def init_ui(self) -> None:
         layout: QVBoxLayout = QVBoxLayout()
 
-        button = QPushButton("A")
-        layout.addWidget(button)
+        scroll_area: QScrollArea = QScrollArea()
+        scroll_area.setWidgetResizable(True)
+        layout.addWidget(scroll_area)
+
+        scroll_content_widget: QWidget = QWidget()
+        scroll_area.setWidget(scroll_content_widget)
+
+        scroll_content_widget_layout: QHBoxLayout = QHBoxLayout()
+        scroll_content_widget.setLayout(scroll_content_widget_layout)
+
+        label = QLabel("Hello World")
+        scroll_content_widget_layout.addWidget(label)
 
         self.setMinimumWidth(self.MINIMUM_WIDTH)
         self.setLayout(layout)
 
 
-class Sidebar(QWidget):
+class MessageList(QWidget):
     MINIMUM_WIDTH_DESKTOP = 150
     MAXIMUM_WIDTH_DESKTOP = 250
     MAXIMUM_WIDTH_MOBILE = MINIMUM_WIDTH_DESKTOP + Main.MINIMUM_WIDTH
@@ -27,17 +103,35 @@ class Sidebar(QWidget):
     def __init__(self) -> None:
         super().__init__()
 
-        layout: QVBoxLayout = QVBoxLayout()
-        self.setLayout(layout)
+        self.init_ui()
 
+    def init_ui(self) -> None:
+        layout: QVBoxLayout = QVBoxLayout()
+
+        scroll_area: QScrollArea = QScrollArea()
+        scroll_area.setWidgetResizable(True)
+        layout.addWidget(scroll_area)
+
+        scroll_content_widget: QWidget = QWidget()
+        scroll_area.setWidget(scroll_content_widget)
+
+        scroll_content_widget_layout: QHBoxLayout = QHBoxLayout()
+        scroll_content_widget.setLayout(scroll_content_widget_layout)
+
+        button: QPushButton = QPushButton("New session")
+        scroll_content_widget_layout.addWidget(button)
+
+        self.setLayout(layout)
         self.setMinimumWidth(self.MINIMUM_WIDTH_DESKTOP)
         self.setMaximumWidth(self.MAXIMUM_WIDTH_DESKTOP)
 
-        button: QPushButton = QPushButton("New session")
-        layout.addWidget(button)
-
 
 class MainWindow(QMainWindow):
+    class Status(Enum):
+        MOBILE: int = 0
+        DESKTOP: int = 1
+        Lock: int = 3
+
     def __init__(self) -> None:
         super().__init__()
 
@@ -45,36 +139,38 @@ class MainWindow(QMainWindow):
         layout: QHBoxLayout = QHBoxLayout()
         widget.setLayout(layout)
 
-        self.sidebar: Sidebar = Sidebar()
+        self.sidebar: MessageList = MessageList()
         layout.addWidget(self.sidebar)
 
         self.main: Main = Main()
         layout.addWidget(self.main)
 
-        self.mobile: bool = False
-        self.lock: bool = False
+        self.status: MainWindow.Status = MainWindow.Status.DESKTOP
 
         self.setMinimumHeight(640)
-        self.setGeometry(0, 0, Sidebar.MAXIMUM_WIDTH_DESKTOP + Main.MINIMUM_WIDTH, 640)
+        self.setGeometry(0, 0, MessageList.MAXIMUM_WIDTH_DESKTOP + Main.MINIMUM_WIDTH, 640)
         self.setCentralWidget(widget)
 
     def resizeEvent(self, event: QResizeEvent):
-        if self.sidebar.width() <= Sidebar.MINIMUM_WIDTH_DESKTOP and not self.mobile:
+        print(self.sidebar.width(), MessageList.MINIMUM_WIDTH_DESKTOP, self.status)
+        if self.sidebar.width() <= MessageList.MINIMUM_WIDTH_DESKTOP and self.status == MainWindow.Status.DESKTOP:
             self.main.hide()
-            self.sidebar.setMaximumWidth(Sidebar.MAXIMUM_WIDTH_MOBILE)
+            self.sidebar.setMaximumWidth(MessageList.MAXIMUM_WIDTH_MOBILE)
             self.setMaximumWidth(self.width())
-            self.mobile, self.lock = True, True
-        elif self.lock and self.sidebar.width() < Sidebar.MAXIMUM_WIDTH_MOBILE:
-            self.lock = False
-        elif self.sidebar.width() >= Sidebar.MAXIMUM_WIDTH_MOBILE and self.mobile and not self.lock:
+            self.status = MainWindow.Status.Lock
+        elif self.status == MainWindow.Status.Lock and self.sidebar.width() < MessageList.MAXIMUM_WIDTH_MOBILE:
+            self.status = MainWindow.Status.MOBILE
+        elif self.sidebar.width() >= MessageList.MAXIMUM_WIDTH_MOBILE and self.status == MainWindow.Status.MOBILE:
             self.main.show()
-            self.sidebar.setMaximumWidth(Sidebar.MAXIMUM_WIDTH_DESKTOP)
+            self.sidebar.setMaximumWidth(MessageList.MAXIMUM_WIDTH_DESKTOP)
             self.setMaximumWidth(16777215)
-            self.mobile = False
+            self.status = MainWindow.Status.DESKTOP
 
 
 if __name__ == '__main__':
     application: QApplication = QApplication(sys.argv)
     main_window: MainWindow = MainWindow()
     main_window.show()
+    # aircraft: Aircraft = Aircraft()
+    # aircraft.show()
     sys.exit(application.exec())
